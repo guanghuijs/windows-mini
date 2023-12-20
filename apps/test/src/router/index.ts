19ac6ec5efc124e5d8be6625bdf3c66dbe7da4e2
吴光辉 <995980020@qq.com>
2023-12-20 18:22:37 +0800

    1

--- apps/test/src/router/index.ts
+++ apps/test/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import type { CreateWindowOptions } from '@packages/layout';
+import type { CreateWindowOptions } from '@packages/types/layout';
 import { menus } from '@app/router/layout';
 import { Desktop, Dashboard, Login } from '@packages/layout';
 
@@ -37,9 +37,11 @@ const router = createRouter({
 
 type A = {
   a: string;
-  b: string;
+  b: number;
   readonly c: string;
 };
+
+export default router;
 
 // function f<T>(a: Partial<T>) {}
 // f<A>({});
@@ -49,10 +51,62 @@ type B = Pick<A, 'a' | 'b'>;
 // function f(a: B) {}
 // f({ a: '', b: '' });
 
-type C = Record<'id', A>;
-function f(a: C) {}
-f({ id: undefined, a: '', b: '' });
+type C = Record<'id', string>;
+
+export const cc: C = { id: '' };
 
 type D = Exclude<A, C>;
 
-export default router;
+type E = keyof A;
+
+type F = {
+  [K in keyof A]: A[K];
+};
+
+const ff: F = { a: '', b: 0, c: '' };
+// Cannot assign to c because it is a read-only property
+// ff.c = '';
+
+type G = {
+  [K in 'a' | 'b' | 'c']: string;
+};
+
+const gg: G = { a: '', b: '', c: '' };
+console.log(gg);
+
+function g1<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+function g2<T>(obj: T, key: keyof T) {
+  return obj[key];
+}
+g1({ name: '', age: '' }, 'name');
+g2({ name: '', age: '' }, 'name');
+
+type GetType<T> = T extends { b: infer R } ? R : T;
+type GetTypeA = GetType<A>;
+
+const typeofTest = { name: '', age: '' };
+export const typeofUse: typeof typeofTest = { name: '', age: '' };
+
+interface HH {
+  name: string;
+  age: number;
+  sex: number;
+}
+
+const hh: Pick<HH, 'name' | 'age'> = { name: '', age: 0 };
+const hhh: Omit<HH, 'name' | 'age'> = { sex: 66 };
+const hhhh: Partial<HH> = { name: '' };
+
+type GetTypeBase = {
+  position: 'left' | 'top';
+  size: 'big' | 'small' | 'middle';
+};
+
+// [P in K]: T[P];
+type getType<T, K extends keyof T> = T extends { [P in K]: infer R } ? R : T;
+type AAA = getType<GetTypeBase, 'position'>;
+type BBB = getType<GetTypeBase, 'size'>;
+const aaa: AAA = 'left';
+const bbb: BBB = 'small';
